f6c84267f2fbd93ef814a5d1ba8292b7545cefbe
yanjingzz <yanjingzz@gmail.com>
2018-12-01 16:37:12 -0800

    Card

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace CardGame
+{
+    [CreateAssetMenu]
+    public class Card : ScriptableObject
+    {
+        public string Title;
+        public string Description;
+        public Sprite Image;
+        public CardType Type;
+        public int Time; // in minutes
+        public int Points;
+    }
+
+    public enum CardType
+    {
+        Art = 0,
+        Gameplay,
+        Feel,
+        Tech,
+        Event
+    }
+}
+
---
+++ Assets/Scripts/CardViz.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CardGame
+{
+    public class CardViz : MonoBehaviour
+    {
+        [SerializeField] Card _card;
+        public Image image;
+        public Text title;
+        public Text points;
+        public Text time;
+        public Text description;
+        public Card Card
+        {
+            get { return _card; }
+            set
+            {
+                _card = value;
+                DisplayCard();
+            }
+        }
+
+        public void DisplayCard ()
+        {
+            if(_card != null)
+            {
+                image.sprite = _card.Image;
+                title.text = _card.Title;
+                description.text = _card.Description;
+                time.text = (_card.Time / 60).ToString();
+                points.text = (_card.Points).ToString();
+
+            }
+        }
+        private void OnValidate()
+        {
+            DisplayCard();
+        }
+    }
+
+}
