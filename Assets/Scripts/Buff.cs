89b863d5d3aa00b7e45512ff0c9eae5088dd9529
littlefriend <gsh0818@126.com>
2018-12-01 20:39:14 -0800

    buffs

---
+++ Assets/Scripts/Buff.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace CardGame
+{
+	[CreateAssetMenu]
+	public class Buff : ScriptableObject
+	{
+		public string Title;
+		public string Description;
+		public BuffType[] Effects;
+	}
+
+	[System.Serializable]
+	public struct BuffType
+	{
+		public int time;
+		public int point;
+	}
+}
